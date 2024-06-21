97a6a69a82760a66e8c49ba58c12476f29ebe931
Hitesh Kumar <hiteshkumar0778@gmail.com>
2024-06-21 13:59:35 +0530

    Update Largest.java

--- Largest.java
+++ Largest.java
@@ -7,19 +7,11 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 		// your code goes here
-		Scanner sc = new Scanner(System.in);
-		
-		int num1 = sc.nextInt();
-		int num2 = sc.nextInt();
-		int num3 = sc.nextInt();
-		
-		if(num1 >= num2 && num1 >= num3){
-		    System.out.println("Max: " + num1);
-		} else if (num2 >= num1 && num2 >= num3){
-		    System.out.println("Max: " + num2);
-		} else {
-		    System.out.println("Max: " + num3);
-		}
-
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        
+        System.out.println(Math.max(c, Math.max(a, b)));
 	}
 }
